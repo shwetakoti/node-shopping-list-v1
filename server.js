a2f13a6235e1630986e713c0dc2e92c8f0dc8c12
shwetakoti <33789453+shwetakoti@users.noreply.github.com>
2018-02-27 21:18:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 
 const express = require('express');
+
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
+
 // we'll use body-parser's json() method to 
 // parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList,Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -29,6 +31,21 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+
+//we are going to add some items to recipies
+
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']) ;
+Recipes.create('smoothie',['milk','strawberry','avacado','banana']);
+Recipes.create('vegetable pasta',['pasta','vegetables','pasta sauce','oilve oil']);
+Recipes.create('broccoli soup',['broccoli','milk','cashews','oilve oil']);
+
+//when a GET request is made to the endpoint, the response should be a list of recipies with in
+//ingredients. The endpoint is /recipes
+
+app.get('/recipes',(req,res) => {
+     res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
